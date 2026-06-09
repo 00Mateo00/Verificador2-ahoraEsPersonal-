2df690c5edc20a244a856280f626e3ca3c974d42
Mateo <mateo.ossa.b@gmail.com>
2026-06-09 18:00:05 -0400

    feat(fix) se corregió el import de los ExcelService

--- app/Models/Actividad.php
+++ app/Models/Actividad.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use app\Services\ExcelService;
+use App\Services\ExcelService;
 
 
 class Actividad extends Model
